dea69bc62fe526b0ed94a7bd277602ec1a93e0d3
Byllie <vantourout.gwendal@gmail.com>
2025-09-18 18:30:04 +0200

    valeur en DB

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.io import wavfile
 
 
-fs, data = wavfile.read("source/6.wav")
+fs, data = wavfile.read("source/1.wav")
 N = len(data)
 T = 1.0 / fs
 y = np.fft.fft(data)
@@ -11,9 +11,9 @@ x = np.fft.fftfreq(N, T)
 n = (x >= 0) & (x <= 10000)
 x = x[n]
 y = np.abs(y[n])
-
+y_db = 20 * np.log10(y) 
 plt.figure(figsize=(10, 6))
-plt.plot(x, y)
+plt.plot(x, y_db)
 
 plt.grid()
 plt.show()
